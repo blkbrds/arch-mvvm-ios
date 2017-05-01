e86234fd3af41b070deea259c3d46eb8fe743c2c
Truong Nguyen V <truong.nguyen@asiantech.vn>
2017-05-01 14:43:17 +0700

    add unit test

--- DemoTests/ViewModelTests/UserViewModelTests.swift
+++ DemoTests/ViewModelTests/UserViewModelTests.swift
@@ -20,12 +20,32 @@ extension LoginViewModel {
 }
 
 class LoginViewModelTests: XCTestCase {
+    
+    func testInitUser() {
+        let user = User()
+        user.login = "truong test"
+        let loginModel = LoginViewModel(user: user)
+        XCTAssertEqual(loginModel.username, "truong test")
+    }
+    
     func testValidateSuccess() {
         let user = LoginViewModel.standard
         let validation = user.validate()
         switch validation {
         case .success: break
         case .failure(_, _):
+            XCTFail("`validation` must be `.success`")
+        }
+    }
+    
+    func testValidateDescriptionSuccess() {
+        let user = LoginViewModel.standard
+        let validation = user.validate()
+        switch validation {
+        case .success:
+            let description = validation.description
+            XCTAssertEqual(description, "Success")
+        case .failure(key: _, msg: _):
             XCTFail("`validation` must be `.success`")
         }
     }
@@ -82,6 +102,23 @@ class LoginViewModelTests: XCTestCase {
                 XCTAssertEqual(error.localizedDescription, "Failure: 'token' too short")
             }
             ex.fulfill()
+        }
+        waitForExpectations(timeout: Timeout.forRequest)
+    }
+    
+    func testLoginFailureInfo() {
+        let expected = expectation(description: "login")
+        let user = LoginViewModel(user: nil)
+        user.username = "at-ios-mvvm"
+        user.token = "001a6476440c30431a17" + "25c310d1abe049189b2a"
+        user.login { (result) in
+            switch result {
+            case .success:
+                XCTFail("Login must be fail")
+            case .failure(let error):
+                XCTAssertEqual(error.code, 401)
+            }
+            expected.fulfill()
         }
         waitForExpectations(timeout: Timeout.forRequest)
     }
