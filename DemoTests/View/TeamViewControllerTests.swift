15d0c6e7dee126096e1da6d34a69be5045465c98
Tien Huynh Q <tien.huynh@asiantech.vn>
2017-06-02 10:37:57 +0700

    fix ci faild

--- DemoTests/View/TeamViewControllerTests.swift
+++ DemoTests/View/TeamViewControllerTests.swift
@@ -14,36 +14,17 @@ import RealmS
 @testable import Demo
 
 class TeamViewControllerTests: XCTestCase {
-    var ex: XCTestExpectation?
-
-    override func setUp() {
-        super.setUp()
-        let nc = NotificationCenter.default
-        nc.addObserver(self, selector: #selector(viewDidUpdated), name: .viewDidUpdated, object: nil)
-    }
-
-    override func tearDown() {
-        let nc = NotificationCenter.default
-        nc.removeObserver(self)
-        super.tearDown()
-    }
 
     func testLoad() {
-        ex = expectation(description: "testLoad")
         let vc = teamViewController()
         vc.viewModel = TeamViewModel.test
         vc.updateView()
-        waitForExpectations(timeout: Timeout.forRequest)
         guard let vmDetail = vc.viewModel.teamDetailViewModel else { return }
         XCTAssertEqual(vc.nameLabel.text, vmDetail.name)
         XCTAssertEqual(vc.slugLabel.text, vmDetail.slug)
         XCTAssertEqual(vc.descLabel.text, vmDetail.desc)
         XCTAssertEqual(vc.memberCountLabel.text, vmDetail.memberCount)
         XCTAssertEqual(vc.repoCountLabel.text, vmDetail.repoCount)
-    }
-
-    @objc private func viewDidUpdated() {
-        ex?.fulfill()
     }
 
     private func teamViewController() -> TeamViewController {
